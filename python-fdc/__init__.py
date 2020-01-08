6e6beefef97789329d2e1fc69e4c019ba0e2cc1a
Nelson-Gon <gonzabato@hotmail.com>
2020-01-08 22:04:12 +0800

    setup automagically

--- python-fdc/__init__.py
+++ python-fdc/__init__.py
@@ -4,3 +4,7 @@ A python interface to the USDA's FoodDataCentral API
 .. author: Nelson Gonzabato
 
 """
+__author__ = "Nelson Gonzabato"
+__version__ = "0.1.0"
+
+
--- python-fdc/setup.py
+++ python-fdc/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(name='pyfdc',
-        version='0.1',
+        version='0.1.0',
         description='A python interface to FoodDataCentral',
         url='http://www.github.com/Nelson-Gon/pyfdc',
         author='Nelson Gonzabato',
         author_email='gonzabato@hotmail.com',
         license='MIT',
-        packages=['python-fdc'],
+        packages=find_packages(),
         install_requires=['requests'],
         zip_safe=False)
