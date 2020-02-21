e5f42b2d2d1d94f9f792fe1a155ab33666330a90
Nelson-Gon <gonzabato@hotmail.com>
2020-02-21 22:04:51 +0800

    Fixes install issues

--- pyfdc/__init__.py
+++ pyfdc/__init__.py
@@ -10,11 +10,11 @@ A python interface to the USDA's FoodDataCentral API
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
+from version import __version__
 
 
 __author__ = "Nelson Gonzabato"
-__version__ = open('version.py').read().rstrip()
+__version__ = __version__
 __all__ = ["pyfdc", "utils"]
 
 
