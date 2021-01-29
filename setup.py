20923d9dbea9dcf1b5cba741625b01f6637a6d7b
Nelson-Gon <gonzabato@hotmail.com>
2021-01-29 17:40:38 +0800

    New release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyfdc',
-      version=open("pyfdc/version.py").read().rstrip(),
+      version="0.2.0",
       description='A python interface to FoodDataCentral',
       url="http://www.github.com/Nelson-Gon/pyfdc",
       download_url="https://github.com/Nelson-Gon/pyfdc/archive/v0.1.2.zip",
@@ -11,7 +11,7 @@ setup(name='pyfdc',
       license='MIT',
       keywords="nutrition food agriculture rest api",
       packages=find_packages(),
-      long_description=open('README.md').read(),
+      long_description=open('README.md', encoding="utf-8").read(),
       long_description_content_type='text/markdown',
       install_requires=['requests', 'pandas'],
       python_requires='>=3.6',
