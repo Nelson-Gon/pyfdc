ec2c1b5bc2e25abbcc113d8e8a44a3595e9757df
Nelson-Gon <gonzabato@hotmail.com>
2021-01-29 17:43:46 +0800

    Use manual versioning for now

--- pyfdc/__init__.py
+++ pyfdc/__init__.py
@@ -12,8 +12,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from version import __version__
+
 
 __author__ = "Nelson Gonzabato"
-__version__ = __version__
 __all__ = ["pyfdc", "utils"]
